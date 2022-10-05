aaefebf3dd2158df53e4b2f94f5a5370ff69040d
Ahmed Mohamed <Ahmed Mohamed>
2022-10-05 04:17:12 +0200

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), mdx()],
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: "dracula",
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: ["javascript"],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+    },
+  },
+});
