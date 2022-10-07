b7dca60f5fe247c5c47810ca4e4418b0340834da
Ahmed Mohamed <Ahmed Mohamed>
2022-10-07 18:07:42 +0200

    add robots.txt file plus add sitemaps to the website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), sitemap()],
   markdown: {
     drafts: true,
     shikiConfig: {
@@ -20,4 +20,6 @@ export default defineConfig({
       wrap: true,
     },
   },
+  site: "https://a7m3d.vercel.app/",
+  integrations: [sitemap()],
 });
