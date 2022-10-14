16ccf182024e5824b948d51b8e0109e84f9e602e
Ahmed Mohamed <Ahmed Mohamed>
2022-10-14 05:02:39 +0200

    fix theme error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
@@ -12,7 +11,7 @@ export default defineConfig({
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-      theme: ayuMirage,
+      theme: "material-darker",
       // Add custom languages
       // Note: Shiki has countless langs built-in, including .astro!
       // https://github.com/shikijs/shiki/blob/main/docs/languages.md
