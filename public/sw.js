63edaac0c4df802efba7cad6eaedc562bdc4e3be
Ahmed Mohamed <Ahmed Mohamed>
2022-11-02 06:59:06 +0200

    add manifest file and app icons

--- public/sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener("install", (event) => {
-  console.log("[Service Worker] Installing ...", event);
-});
-
-self.addEventListener("activate", (event) => {
-  console.log("[Service Worker] Installing ...", event);
-  return self.clients.claim();
-});
